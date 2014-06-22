92491af42ee9e53b9f3b1f74d8a1fe55a8b5a0dc
wzaghal <wisamzaghal@gmail.com>
2014-06-22 00:24:45 -0400

    integrate leap movement

--- app/scripts/leap.js
+++ app/scripts/leap.js
@@ -1,68 +1,54 @@
 'use strict'
 var debug;
 
+$(document).ready(function(){
 
-//left-hand object
-var left = (function() {
-    var left = [[],[],[],[],[]];
 
-    return {
-        setFinger: function( finger, valArray ){
-            left[finger] = valArray;
 
-        },
-        getFinger: function(finger){
-            return left[finger];
-        }
-    };
+    //Leap.loop uses websockets
 
-}());
+    Leap.loop(function(frame) {
+        //for debug purposes
+        debug = frame;
+        var finger;
 
-//right-hand object
-var right = (function() {
-    var right = [[],[],[],[],[]];
+        frame.hands.forEach(function(hand, index) {
 
-    return {
-        setFinger: function( finger, valArray ){
-            right[finger] = valArray;
+            //loop through each finger
+            for (var i = 0, max = hand.fingers.length; i < max; i++){
+                finger = hand.fingers[i];
 
-        },
-        getFinger: function(finger){
-            return right[finger];
-        },
-    };
-
-}());
-
-//Leap.loop uses websockets
-
-Leap.loop(function(frame) {
-    //for debug purposes
-    debug = frame;
-    var finger;
-
-    frame.hands.forEach(function(hand, index) {
-
-        //loop through each finger
-        for (var i = 0, max = hand.fingers.length; i < max; i++){
-            finger = hand.fingers[i];
-
-            //save extended fingers x,y,z positions
-            if (finger.extended){
+                //save extended fingers x,y,z positions
                 //Debug to console
-              //  console.log(finger);
+                //console.log(finger);
 
                 if (hand.type === "left"){
-
-                    left.setFinger(finger.type, finger.tipPosition);
+                    if (finger.extended){
+                        left.setFinger(finger.type, finger.tipPosition);
+                    }else{
+                        left.setFinger(finger.type,[0,0,0])
+                    }
 
                 }else if (hand.type === "right"){
+                     if (finger.extended){
+                         right.setFinger(finger.type, finger.tipPosition);
+                    }else{
 
-                    right.setFinger(finger.type, finger.tipPosition);
+                    }
                 }
-            }//if
-        } //for
 
-    });
+            } //for
+            updateFingers();
+            $('.finger1').text(left.getFinger(0));
 
-}).use('screenPosition', {scale: 0.50});
+        });
+        if (frame.hands.length === 0){
+             for (var i = 0, max = 5; i < max; i++){
+                left.setFinger(i,[0,0,0]);
+                $('.finger1').text(left.getFinger(0));
+
+             }
+        }
+
+    }).use('screenPosition', {scale: 0.50});
+});
