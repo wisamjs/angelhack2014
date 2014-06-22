d75f4e5a915aa7ea23571efd28f6bcfdc1a6ef73
Elizabeth Melito <melito.elizabeth@gmail.com>
2014-06-21 21:16:22 -0400

    moving!

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var canvas=document.getElementById('myCanvas');
 var ctx=canvas.getContext('2d');
 document.body.appendChild(canvas);
+var debug;
 var lives = 3;
 var fall = true;
 var fingers = [];
 var bugs = [];
+var then = Date.now();
 // var bugImage = new Image();
 // var fingerImage = new Image();
 
 
-// var background = new Image();
-// background.src = "img/sky.jpg";
+var background = new Image();
+background.src = "img/sky.jpg";
 // background.onload = function () {[]
 // 	ctx.drawImage(background,0,0);
 // };
@@ -26,13 +28,30 @@ var init = function(){
 		var finger = {
 		posX: i*120,
 		posY: 500	
-		}
+		};
 		fingers.push(finger);
 	};
-	initBug(fingers);
+
+
+	main(fingers);
 };
 
-var initBug = function(fingers){
+var main = function(fingers){
+	var now = Date.now();
+	var delta = now - then;
+
+	initBug();
+	update(delta / 1000);
+	render();
+
+	then = now;
+
+	requestAnimationFrame(main);
+};
+
+
+
+var initBug = function(){
 	for(var i=0;i<3;i++){
 		var bug = {
 			speedX: 120, // movement in pixels per second
@@ -41,25 +60,26 @@ var initBug = function(fingers){
 			posY: Math.floor(Math.random()*(600))
 		};
 
+		console.log(bug.posX, bug.posY);
 		bug.speedX = Math.floor(Math.random()*(256+256)-256);
 		bugs.push(bug);
 		}
-
-	update(bugs, fingers);
 };
 
-var update = function(bugs, fingers){
-	for(var i=0;i<bugs.length;i++){
-		bugs[i].posX += bugs[i].posX*bugs[i].speedX;
+var update = function(time){
 
-		while (fall){
+	for(var i=0;i<bugs.length;i++){
+		bugs[i].posX = bugs[i].posX+bugs[i].speedX*time;
+
+		while(fall){
+			
 			for(var j=0;j<fingers.length;j++){
-				if(bugs[i].posX >= (fingers[j].posX + 20) || bugs[i].posX <= (fingers[j].posX - 20)){
+				if(bugs[i].posX <= (fingers[j].posX + 20) || bugs[i].posX >= (fingers[j].posX - 20)){
 					bugs[i].posY = fingers[j].posY;
 					fall = false;
 				}
 				else {
-					bugs[i].posY += bugs[i].speedY;
+					bugs[i].posY = bugs[i].posY + bugs[i].speedY*time;
 				}
 			}
 			console.log(bugs[i].posY);
@@ -78,17 +98,17 @@ var update = function(bugs, fingers){
 	render(bugs, fingers);
 };
 
-var render = function(bugs, fingers) {
-	// ctx.drawImage(background, 0, 0);
+var render = function() {
+	ctx.drawImage(background, 0, 0);
 
 	// ctx.drawImage(bugImage, bug.posX, bug.posY);
+
 	for(var i=0;i<bugs.length;i++){
 		ctx.fillStyle="rgb(100,100,100)";
 		ctx.fillRect(bugs[i].posX,bugs[i].posY,50,50);
-		console.log(bugs[i].posX, bugs[i].posY);
-	};
+	}
 
-	for (var j=0;j<fingers.length;j++){
+	for(var j=0;j<fingers.length;j++){
 		// ctx.drawImage(fingerImage, fingers[j].posX, fingers[j].posY);
 		ctx.fillStyle="hsl(100, 100%, 60%)";
 		ctx.fillRect(fingers[j].posX,fingers[j].posY,100,100);
@@ -99,7 +119,9 @@ var render = function(bugs, fingers) {
 	// ctx.font = "24px Helvetica";
 	// ctx.textAlign = "left";
 	// ctx.textBaseline = "top";
-	// ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
+	// ctx.fillText("Lives: " + lives, 32, 32);
 };
 
+
 init();
+
